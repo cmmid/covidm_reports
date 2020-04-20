e1b36397b46e87299b3e5438647502d55708cabc
Kevin van Zandvoort <Kevin.Van-Zandvoort@lshtm.ac.uk>
2020-04-20 12:59:17 +0100

    Adjust severity estimates for LMICs

--- create_params_set.R
+++ create_params_set.R
@@ -27,32 +27,42 @@ if (!length(matref)) matref <- country
 
 stopifnot(length(country)==1)
 
+#set up node-runs
 probs = fread(
-  "Age,Prop_symptomatic,IFR,Prop_inf_hosp,Prop_inf_critical,Prop_critical_fatal,Prop_noncritical_fatal,Prop_symp_hospitalised,Prop_hospitalised_critical
-10,0.66,8.59E-05,0.002361009,6.44E-05,0.5,0,0,0.3
-20,0.66,0.000122561,0.003370421,9.19E-05,0.5,9.47E-04,0.007615301,0.3
-30,0.66,0.000382331,0.010514103,0.000286748,0.5,0.001005803,0.008086654,0.3
-40,0.66,0.000851765,0.023423527,0.000638823,0.5,0.001231579,0.009901895,0.3
-50,0.66,0.001489873,0.0394717,0.001117404,0.5,0.002305449,0.018535807,0.3
-60,0.66,0.006933589,0.098113786,0.005200192,0.5,0.006754596,0.054306954,0.3
-70,0.66,0.022120421,0.224965092,0.016590316,0.5,0.018720727,0.150514645,0.3
-80,0.66,0.059223786,0.362002579,0.04441784,0.5,0.041408882,0.332927412,0.3
-100,0.66,0.087585558,0.437927788,0.065689168,0.5,0.076818182,0.617618182,0.3"
+  "Age_low,Age_high,Prop_symptomatic,IFR,Prop_inf_hosp,Prop_inf_critical,Prop_critical_fatal,Prop_noncritical_fatal,Prop_symp_hospitalised,Prop_hospitalised_critical
+0,9,0.66,8.59E-05,0.002361009,6.44E-05,0.5,0,0,0.3
+10,19,0.66,0.000122561,0.003370421,9.19E-05,0.5,9.47E-04,0.007615301,0.3
+20,29,0.66,0.000382331,0.010514103,0.000286748,0.5,0.001005803,0.008086654,0.3
+30,39,0.66,0.000851765,0.023423527,0.000638823,0.5,0.001231579,0.009901895,0.3
+40,49,0.66,0.001489873,0.0394717,0.001117404,0.5,0.002305449,0.018535807,0.3
+50,59,0.66,0.006933589,0.098113786,0.005200192,0.5,0.006754596,0.054306954,0.3
+60,69,0.66,0.022120421,0.224965092,0.016590316,0.5,0.018720727,0.150514645,0.3
+70,79,0.66,0.059223786,0.362002579,0.04441784,0.5,0.041408882,0.332927412,0.3
+80,100,0.66,0.087585558,0.437927788,0.065689168,0.5,0.076818182,0.617618182,0.3"
 )
 
-<<<<<<< HEAD
-#set up node-runs
-seed_cases <- 50
-=======
-reformat = function(P) {
-  # no info to re-weight these, so assume 70-74 is like 70-79, and 75+ is like 80+
-  return (c(rep(P[1:7], each = 2),P[8:9]))
-}
->>>>>>> master
+#increase CFR
+cfr_RR <- 1.5
+probs[, Prop_critical_fatal := Prop_critical_fatal * cfr_RR]
+probs[, Prop_noncritical_fatal := Prop_noncritical_fatal * cfr_RR]
 
-P.icu_symp     = reformat(probs[, Prop_symp_hospitalised * Prop_hospitalised_critical]);
-P.nonicu_symp  = reformat(probs[, Prop_symp_hospitalised * (1 - Prop_hospitalised_critical)]);
-P.death    = reformat(probs[, Prop_noncritical_fatal]);
+#min(1, x) does not work for noncritical_fatal, for some reason
+probs[Prop_critical_fatal > 1, Prop_critical_fatal := 1]
+probs[Prop_noncritical_fatal > 1, Prop_noncritical_fatal := 1]
+
+reformat = function(P, lmic_adjust=TRUE) {
+  # no info to re-weight these, so assume 70-74 is like 70-79, and 75+ is like 80+
+  if(lmic_adjust){
+    P <- P[2:length(P)]
+    return (rep(P[1:8], each = 2))
+  } else {
+    return (c(rep(P[1:7], each = 2),P[8:9])) 
+  }
+}
+
+P.icu_symp     = reformat(probs[, Prop_symp_hospitalised * Prop_hospitalised_critical], lmic_adjust = TRUE);
+P.nonicu_symp  = reformat(probs[, Prop_symp_hospitalised * (1 - Prop_hospitalised_critical)], lmic_adjust = TRUE);
+P.death    = reformat(probs[, Prop_noncritical_fatal], lmic_adjust = TRUE);
 
 max_time <- 60
 tres <- 0.25
@@ -92,7 +102,6 @@ cm_track_process <- function(src, name, delays, agecats = 16, report = "p") {
     delays = t(delays)
   )
 }
-
 
 burden_processes = list(
   # process of sending symptomatic cases to hospital icu or ward
